515279a90c5487ba25f728c03cde32043213f214
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2024-03-20 15:37:23 +0100

    scraper functionality

---
+++ scraper.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const scrapeWebsite = async () => {
+  try {
+    const response = await axios.get("https://www.asqin.org");
+    const $ = cheerio.load(response.data);
+    const minimumZakahValue = $(
+      'tr:contains("Minimum Zakkah") td:nth-child(2)'
+    ).text();
+    console.log("Minimum Zakah Value:", minimumZakahValue);
+    return minimumZakahValue;
+  } catch (error) {
+    console.log("Error scraping data", error);
+    return null;
+  }
+};
+
+scrapeWebsite();
