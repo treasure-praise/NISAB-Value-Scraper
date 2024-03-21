def3b0007e76dc1140475a470f75778171f10eb1
treasure <treasurepraiseolatunbosun@yahoo.com>
2024-03-21 16:55:06 +0100

    Update cron.js
    Updated cron job to run every Sunday at 12:00 PM (noon)

--- cron.js
+++ cron.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const scrapeWebsite = require("./scraper");
 const postData = require("./postData");
 
-cron.schedule("* * * * *", async () => {
+cron.schedule('0 12 * * 0', async () => {
   console.log("Running scraping and posting job...");
   const scrapedData = await scrapeWebsite();
   if (scrapedData) {
